00107125db5114848364c4ce0e001f1abf81334d
Darren Merrett <darren@z5.ltd>
2018-02-07 19:42:27 +0000

    KashCoin network settings

---
+++ kashcoinNetwork.js
@@ -0,0 +1,13 @@
+module.exports =  {
+	kashcoin: {
+	    messagePrefix: '\x18Kashcoin Signed Message:\n',
+	    bech32: 'bc',
+	    bip32: {
+	      public: 0x0247455F,
+	      private: 0x023267AB
+	    },
+	    pubKeyHash: 0x2b,
+	    scriptHash: 0x14,
+	    wif: 0xbb
+	},
+};
